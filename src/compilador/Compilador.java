ed69ca5ff31117aadc5b14db92cd3f9505e6b847
HectorRamosP <hetorbeto1@gmail.com>
2025-09-15 11:07:14 -0700

    Primer commit

---
+++ src/compilador/Compilador.java
@@ -0,0 +1,15 @@
+package compilador;
+
+import java.io.IOException;
+
+public class Compilador {
+
+    nodo p;
+
+    public static void main(String[] args) throws IOException {
+        lexico lexico = new lexico();
+        if (!lexico.errorEncontrado) {
+            System.out.println("Proceso de Compilacion Completado");
+        }
+    }
+}
